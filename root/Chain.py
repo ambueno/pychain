eaf94b7377e3682407a5e0733f38a83c7a667498
ambueno <amandabuenocampos@gmail.com>
2022-02-08 19:54:13 -0300

    Add files via upload

--- root/Chain.py
+++ root/Chain.py
@@ -1,14 +1,26 @@
 from hashlib import sha256
-
 import json
 import time
 
 
-class Chain:
+class Chain(object):
     def __init__(self):
         self.blockchain = []
-        self.pending = []
-        self.add_block(prevhash="Genesis", proof=123)
+        self.pending_transactions = []
+        self.add_block(previous_hash="The Times 03/09/2009 Chancellor on brink of second bailout for banks.", proof=123)
+
+    def add_block(self, proof, previous_hash=None):
+        block = {
+            "index": len(self.blockchain),
+            "timestamp": time.time(),
+            "transactions": self.pending_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.blockchain[-1]),
+        }
+        self.pending_transactions = []
+        self.blockchain.append(block)
+
+        return block
 
     def add_transaction(self, sender, recipient, amount):
         transaction = {
@@ -16,20 +28,43 @@ class Chain:
             "recipient": recipient,
             "amount": amount
         }
-        self.pending.append(transaction)
+        self.pending_transactions.append(transaction)
 
-    def compute_hash(self, block):
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.blockchain[-1]
+
+    @staticmethod
+    def hash(block):
         json_block = json.dumps(block, sort_keys=True).encode()
-        current_hash = sha256(json_block).hexdigest()
-        return current_hash
+        return sha256(json_block).hexdigest()
 
-    def add_block(self, proof, prevhash=None,):
-        block = {
-            "index": len(self.blockchain),
-            "timestamp": time.time(),
-            "transactions": self.pending,
-            "proof": proof,
-            "prevhash": prevhash or self.compute_hash(self.blockchain[-1])
-        }
-        self.pending = []
-        self.blockchain.append(block)
+    def proof_of_work(self, previous_proof):
+        proof = 0
+        while self.check_proof(previous_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def check_proof(previous_proof, proof):
+        if (sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()) == '0000':
+            return True
+
+    def valid_proof(self, blockchain):
+        previous_block = self.blockchain[0]
+        block_index = 0
+
+        while block_index < len(blockchain) - 1:
+            block = blockchain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            current_proof = block['proof']
+            if (sha256(str(current_proof ** 2 - previous_proof ** 2).encode()).hexdigest()) != '0000':
+                return False
+
+            previous_block = block
+            block_index += 1
+        return True
--- root/main.py
+++ root/main.py
@@ -2,21 +2,20 @@ from Chain import Chain
 
 chain = Chain()
 
-transaction1 = chain.add_transaction("Vitalik", "Satoshi", 100)
+transaction1 = chain.add_transaction("Amanda", "Satoshi", '70 AC')
 
-transaction2 = chain.add_transaction("Satoshi", "Alice", 10)
+transaction2 = chain.add_transaction("Satoshi", "Java", '23 AC')
 
-transaction3 = chain.add_transaction("Alice", "Charlie", 34)
+transaction3 = chain.add_transaction("Java", "Lucas", '34 AC')
 
 chain.add_block(12345)
 
-transaction4 = chain.add_transaction("Bob", "Eve", 23)
+transaction4 = chain.add_transaction("Ullas", "Benvas", '11 AC')
 
-transaction5 = chain.add_transaction("Dennis", "Brian", 3)
+transaction5 = chain.add_transaction("Leander", "Bryan", '3 AC')
 
-transaction6 = chain.add_transaction("Ken", "Doug", 88)
+transaction6 = chain.add_transaction("Marcus", "Honda", '88 AC')
 
 chain.add_block(6789)
 
 print(chain.blockchain)
-
